a23a904a0ca90bd563f09663b9c2b01399bce5d6
Marium-pixel <imranmarium742@gmail.com>
2025-11-23 19:30:48 +0500

    Create Difficulty.h

---
+++ PacMan-DSA-Project/Difficulty.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef DIFFICULTY_H
+#define DIFFICULTY_H
+#include "raylib.h"
+
+
+
+// Draw level selection screen
+void DrawLevelSelectScreen(int winW, int winH, Font font, int selected);
+
+#endif
